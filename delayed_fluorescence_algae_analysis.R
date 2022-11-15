eb1211124a871da01c48d5315c79f6bd3b0a4d11
Samuel R dos Santos <hidroqualis@gmail.com>
2022-11-15 11:57:08 -0300

    Initial commit: delayed fluorescence code

---
+++ delayed_fluorescence_algae_analysis.R
@@ -0,0 +1,200 @@
+# INSTAL AND LOAD R PACKAGES ---------------------------------------------------
+if (!require(svDialogs)) install.packages("svDialogs")
+if (!require(readr)) install.packages("readr")
+if (!require(writexl)) install.packages("writexl")
+if (!require(readxl)) install.packages("readxl")
+if (!require(tidyr)) install.packages("tidyr")
+if (!require(tidyverse)) install.packages("tidyverse")
+if (!require(ggplot2)) install.packages("ggplot2")
+if (!require(ggpubr)) install.packages("ggpubr")
+if (!require(scales)) install.packages("scales")
+if (!require(dplyr)) install.packages("dplyr")
+if (!require(matrixStats)) install.packages("matrixStats")
+if (!require(purrr)) install.packages("purrr")
+if (!require(stringr)) install.packages("stringr")
+
+library(svDialogs)
+library(readr)
+library(writexl)
+library(readxl)
+library(tidyr)
+library(tidyverse)
+library(ggplot2)
+library(ggpubr)
+library(scales)
+library(datasets)
+library(dplyr)
+library(magrittr)
+library(purrr)
+
+#SET WORKING DIRECTORY
+getwd()
+path_with_excel_files <- setwd(dlg_dir(default = getwd())$res)
+path_with_excel_files
+getwd()
+
+# READ AND JOIN MULTIPLE EXCEL FILES -------------------------------------------
+
+# get a list of files
+
+dir(path_with_excel_files, full.names = T, pattern = ".xls")
+filenames <- list.files(pattern = '\\.xls', full.names = TRUE)
+filenames
+
+#DELAYED FLUORESCENCE EXPERIMENT PROPERTIES
+#Sample properties
+
+sampleName <- dlg_input(message = "Inform the name of chemical solution", gui = .GUI)$res
+numberSamples <- as.integer(dlg_input(message = "Inform the number of samples", gui = .GUI)$res)
+numberReplicates <- as.integer(dlg_input(message = "Inform the number of replicates", gui = .GUI)$res)
+led_setup <- dlg_input(message = "Type the LED setup for delayed fluorescence in PMX, e.g. L01S03R01", gui = .GUI)$res
+cat(paste('Chemical tested:' , sampleName, '\n', 
+          'Number of samples:', numberSamples, '\n',
+          'Number of replicates:', numberReplicates, '\n',
+          'PMX setup:', led_setup))
+
+# Importing all excel files into one data frame
+
+all_excel_files <- path_with_excel_files %>%
+  list.files(pattern = ".xls", full.names = TRUE) %>%
+  set_names(filenames) %>% 
+  map_dfr(read_excel, sheet = 2, .id = "filename") %>% 
+  mutate(filename = basename(filename)) %>% 
+  pivot_wider(names_from = filename, values_from = L01S03R01)
+all_excel_files
+
+#Cuting the noise background
+
+delayed_lum_data <- filter(all_excel_files, Time > 0.9)
+delayed_lum_data
+
+#DELAYED FLUORESCENCE PLOT DEFAULT
+
+plot(delayed_lum_data$Time, delayed_lum_data$`0.0(1).xls`,
+     type = "l", col="dark green", 
+     ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`0.5(1).xls`, 
+      type = "l", col="blue", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`1.5(1).xls`, 
+      type = "l", col="orange", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`4.5(1).xls`, 
+      type = "l", col="cyan", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`13.5(1).xls`, 
+      type = "l", col="red", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`40.5(1).xls`, 
+      type = "l", col="black", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+
+#graph[2]
+plot(delayed_lum_data$Time, delayed_lum_data$`0.0(2).xls`,
+     type = "l", col="dark green", 
+     ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`0.5(2).xls`, 
+      type = "l", col="blue", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`1.5(2).xls`, 
+      type = "l", col="orange", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`4.5(2).xls`, 
+      type = "l", col="cyan", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`13.5(2).xls`, 
+      type = "l", col="red", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`40.5(2).xls`, 
+      type = "l", col="black", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+
+#graph[3]
+plot(delayed_lum_data$Time, delayed_lum_data$`0.0(3).xls`,
+     type = "l", col="dark green", 
+     ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`0.5(3).xls`, 
+      type = "l", col="blue", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`1.5(3).xls`, 
+      type = "l", col="orange", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`4.5(3).xls`, 
+      type = "l", col="cyan", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`13.5(3).xls`, 
+      type = "l", col="red", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+lines(delayed_lum_data$Time, delayed_lum_data$`40.5(3).xls`, 
+      type = "l", col="black", 
+      ylim = c(1e2,1e6), xlab = "Time [s]", ylab = "photon-counts")
+
+#DELAYED FLUORESCENCE GGPLOT---------------------------------------------------
+
+df_plot <- setwd(dlg_dir(default = getwd())$res)%>%
+  list.files(pattern = ".xls", full.names = TRUE) %>%
+  set_names(filenames) %>% 
+  map_dfr(read_excel, sheet = 2, .id = "filename") %>% 
+  mutate(filename      = basename(filename)) %>% 
+  mutate(concentration = str_extract(filename, pattern = "[^(]+")) %>% 
+  mutate(replicate     = str_extract(filename, pattern = "(?<=\\().+?(?=\\))")) %>%
+  rename(value = L01S03R01) %>% 
+  select(filename, concentration, replicate, Time, value) %>% 
+  arrange(filename, concentration, replicate, Time)
+df_plot
+
+sapply(df_plot, class)
+df_plot$concentration <- as.numeric(df_plot$concentration)
+sapply(df_plot, class)
+
+#Plot individual data
+df_plot%>%
+  subset(Time > 1.0) %>%
+  ggplot(aes(x = Time, y = value, color = concentration, group = replicate)) +
+  theme_bw() +
+  geom_hline(yintercept = 0) +
+  geom_line() +
+  ylim(1e2,1e6)+
+  facet_grid(replicate ~ concentration)+
+  labs(title = "Delayed Fluorescence",
+       x = "Time [s]",
+       y = "photon-counts")
+
+#Plot average
+df_plot %>%
+  subset(Time > 1.0) %>%
+  ggplot(aes(x = Time, y = value, color = concentration))+
+  theme_bw()+
+  ylim(1e2,1e6)+
+  geom_hline(yintercept = 0) +
+  stat_summary(fun.data = mean_se, geom = "line") +
+  facet_grid(. ~ concentration)+
+  labs(title = "Mean of Delayed Fluorescence",
+       x = "Time [s]",
+       y = "photon-counts")
+
+#Sum of photon-counts
+df_plot %>%
+  subset(Time > 1.0) %>%
+  ggplot(aes(x = concentration, y = value, color = concentration)) +
+  theme_bw() +
+  geom_hline(yintercept = 0) +
+  stat_summary(fun = sum, geom = "bar") +
+  facet_grid(. ~ replicate)+
+  labs(title = "Total counts of delayed fluorescence",
+       x = "concentration",
+       y = "total counts")
+
+#Concentration vs total counts
+df_plot %>%
+  subset(Time > 0.9) %>%
+  mutate(concentration = as.numeric(as.character(concentration))) %>% 
+  group_by(concentration) %>% 
+  summarise(total_counts = sum(value)) %>% 
+  ggplot(aes(x = concentration, y = total_counts)) +
+  geom_point() +
+  geom_smooth(method = lm) +
+  stat_cor(method="pearson", label.x = 25)
+
+#STATISTICS ANALYSIS -----------------------------------------------------------
+
